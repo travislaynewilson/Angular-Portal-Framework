883f828ab52219eb7c06b642477ee8e304fba68f
Travis Wilson <travis.layne.wilson@gmail.com>
2017-12-20 11:20:28 -0600

    + Added some drawer demos

--- src/app/demo/sidenavs/sidenavs.component.ts
+++ src/app/demo/sidenavs/sidenavs.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidenavsComponent implements OnInit {
 
 	mode: 'push' | 'over' | 'side' = 'side';
+	carddrawermode:  'push' | 'over' | 'side' = 'side';
 
 	constructor () { }
 
